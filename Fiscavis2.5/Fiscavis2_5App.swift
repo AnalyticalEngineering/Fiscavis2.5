c0bca15f031593af8fd7d152abbd175f783fa242
Joseph DeWeese <jdeweese@weldtascics.systems>
2023-11-19 16:58:03 -0600

    Initial Commit

---
+++ Fiscavis2.5/Fiscavis2_5App.swift
@@ -0,0 +1,17 @@
+//
+//  Fiscavis2_5App.swift
+//  Fiscavis2.5
+//
+//  Created by J. DeWeese on 11/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Fiscavis2_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
